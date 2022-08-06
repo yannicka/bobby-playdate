a8a5d366fec4c7305b26426591409925a4c5b3e8
Yannick Armand <yannick.armand@protonmail.com>
2022-08-06 14:52:43 +0200

    Ascenseur dans page d'aide

--- Source/scenes/help.lua
+++ Source/scenes/help.lua
@@ -1,29 +1,61 @@
 class('HelpScene').extends(Scene)
 
+local text = [[
+*Instructions*
+
+_Keys_
+Move the character with the directional arrows
+
+_Goal_
+Complete all levels
+For each level, collect all the coins and then reach the exit.
+
+_Blocks_
+The rocks block you
+The treadmills take you in a given direction.
+Buttons only allow you a limited number of passages.
+Turnstiles block you in certain directions and turn clockwise when you exit.
+Ice slides you to the next void or solid block you encounter.
+]]
+
+local offset = 0
+
 function HelpScene:init()
     HelpScene.super.init(self)
 
-    playdate.graphics.setDrawOffset(0, 0)
+    playdate.display.setOffset(0, 0)
 end
 
 function HelpScene:update()
-    playdate.graphics.drawText('Keys', 20, 20)
-    playdate.graphics.drawText('Move the character with the directional arrows.', 20, 40)
+    local gridviewImage = playdate.graphics.image.new(playdate.display.getWidth() - 20, 500)
+    playdate.graphics.pushContext(gridviewImage)
+    playdate.graphics.drawTextInRect(text, 10, 10, playdate.display.getWidth() - 20, 500, 5)
+    playdate.graphics.popContext()
+    -- self.gridviewSprite:setImage(gridviewImage)
+    gridviewImage:draw(0, offset)
 
-    playdate.graphics.drawText('Goal', 20, 60)
-    playdate.graphics.drawText('Complete all levels.', 20, 80)
-    playdate.graphics.drawText('For each level, collect all the coins and then reach the exit.', 20, 100)
+    if playdate.buttonIsPressed(playdate.kButtonUp) then
+        offset += 10
 
-    playdate.graphics.drawText('Blocks', 20, 120)
-    playdate.graphics.drawText('The rocks block you.', 20, 140)
-    playdate.graphics.drawText('The treadmills take you in a given direction.', 20, 160)
-    playdate.graphics.drawText('Buttons only allow you a limited number of passages.', 20, 180)
-    playdate.graphics.drawText('Turnstiles block you in certain directions and turn clockwise when you exit.', 20, 200)
-    playdate.graphics.drawText('Ice slides you to the next void or solid block you encounter.', 20, 220)
+        if offset > 0 then
+            offset = 0
+        end
+    end
+
+    if playdate.buttonIsPressed(playdate.kButtonDown) then
+        offset -= 10
+
+        if offset < -140 then
+            offset = -140
+        end
+    end
 
     if playdate.buttonJustPressed(playdate.kButtonB) then
         changeScene(HomeScene)
     end
+
+    playdate.graphics.sprite.update()
+    playdate.timer.updateTimers()
 end
 
 function HelpScene:destroy()
