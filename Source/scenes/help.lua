933e487e27c14227d5968a84319f8758450a76b9
Yannick Armand <yannick.armand@protonmail.com>
2022-08-06 13:43:16 +0200

    Première version de la page d'aide

--- Source/scenes/help.lua
+++ Source/scenes/help.lua
@@ -6,9 +6,20 @@ end
 
 function HelpScene:update()
     playdate.graphics.setDrawOffset(0, 0)
-    
-    local playButton = ScreenButton('Page d\'aide', 20, 20, 10)
-    playButton:render()
+
+    playdate.graphics.drawText('Keys', 20, 20)
+    playdate.graphics.drawText('Move the character with the directional arrows.', 20, 40)
+
+    playdate.graphics.drawText('Goal', 20, 60)
+    playdate.graphics.drawText('Complete all levels.', 20, 80)
+    playdate.graphics.drawText('For each level, collect all the coins and then reach the exit.', 20, 100)
+
+    playdate.graphics.drawText('Blocks', 20, 120)
+    playdate.graphics.drawText('The rocks block you.', 20, 140)
+    playdate.graphics.drawText('The treadmills take you in a given direction.', 20, 160)
+    playdate.graphics.drawText('Buttons only allow you a limited number of passages.', 20, 180)
+    playdate.graphics.drawText('Turnstiles block you in certain directions and turn clockwise when you exit.', 20, 200)
+    playdate.graphics.drawText('Ice slides you to the next void or solid block you encounter.', 20, 220)
 
     if playdate.buttonJustPressed(playdate.kButtonB) then
         changeScene(HomeScene)
