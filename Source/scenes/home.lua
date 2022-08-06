f253cea96b81a6ce9352a2fd1df3ceecc7a88bd1
Yannick Armand <yannick.armand@protonmail.com>
2022-08-06 15:28:13 +0200

    Boutons d'accueil

--- Source/scenes/home.lua
+++ Source/scenes/home.lua
@@ -4,28 +4,82 @@ function HomeScene:init()
     HomeScene.super.init(self)
 
     self.titleImage = playdate.graphics.image.new('img/title')
+    self.buttonSelected = 'play'
+
+    self.playButton = ScreenButton('*Play*', 200, 140, 10)
+    self.playButton.selected = true
+
+    self.creditsButton = ScreenButton('Credits', 20, 202, 10)
+    self.helpButton = ScreenButton('Instructions', 170, 202, 10)
+    self.optionsButton = ScreenButton('Options', 328, 202, 10)
 end
 
 function HomeScene:update()
     playdate.graphics.setDrawOffset(0, 0)
 
     self.titleImage:draw(120, 16)
-    
-    local playButton = ScreenButton('*Play*', 200, 140, 10)
-    playButton.selected = true
-    playButton:render()
 
-    local creditsButton = ScreenButton('Credits', 20, 202, 10)
-    creditsButton:render()
+    self.playButton:render()
+    self.creditsButton:render()
+    self.helpButton:render()
+    self.optionsButton:render()
 
-    local helpButton = ScreenButton('Instructions', 170, 202, 10)
-    helpButton:render()
+    if playdate.buttonJustPressed(playdate.kButtonDown) then
+        if self.buttonSelected == 'play' then
+            self.playButton.selected = false
+            self.helpButton.selected = true
 
-    local optionsButton = ScreenButton('Options', 328, 202, 10)
-    optionsButton:render()
+            self.buttonSelected = 'help'
+        end
+    end
+
+    if playdate.buttonJustPressed(playdate.kButtonUp) then
+        self.playButton.selected = true
+        self.creditsButton.selected = false
+        self.helpButton.selected = false
+        self.optionsButton.selected = false
+
+        self.buttonSelected = 'play'
+    end
+
+    if playdate.buttonJustPressed(playdate.kButtonLeft) then
+        if self.buttonSelected == 'help' then
+            self.creditsButton.selected = true
+            self.helpButton.selected = false
+
+            self.buttonSelected = 'credits'
+        elseif self.buttonSelected == 'options' then
+            self.helpButton.selected = true
+            self.optionsButton.selected = false
+
+            self.buttonSelected = 'help'
+        end
+    end
+
+    if playdate.buttonJustPressed(playdate.kButtonRight) then
+        if self.buttonSelected == 'credits' then
+            self.creditsButton.selected = false
+            self.helpButton.selected = true
+
+            self.buttonSelected = 'help'
+        elseif self.buttonSelected == 'help' then
+            self.helpButton.selected = false
+            self.optionsButton.selected = true
+
+            self.buttonSelected = 'options'
+        end
+    end
 
     if playdate.buttonJustPressed(playdate.kButtonA) then
-        changeScene(LevelSelectorScene)
+        if self.buttonSelected == 'play' then
+            changeScene(LevelSelectorScene)
+        elseif self.buttonSelected == 'credits' then
+            changeScene(CreditsScene)
+        elseif self.buttonSelected == 'help' then
+            changeScene(HelpScene)
+        elseif self.buttonSelected == 'options' then
+            changeScene(OptionsScene)
+        end
     end
 end
 
