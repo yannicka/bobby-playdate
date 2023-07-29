b5bf1dcf95b326cf575e9597fb51ccece534dc51
Yannick Armand <yannick.armand@protonmail.com>
2023-07-29 22:13:48 +0200

    Suppression de code mort

--- src/scenes/scene.lua
+++ src/scenes/scene.lua
@@ -3,11 +3,3 @@ class('Scene').extends(Object)
 function Scene:init()
     Scene.super.init(self)
 end
-
-function Cell:update()
-    -- À surcharger
-end
-
-function Cell:destroy()
-    -- À surcharger
-end
